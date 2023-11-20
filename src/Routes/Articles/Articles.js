70defabc3d095f792fcd19317713ab4834918285
Wade Butler <wadebutler@Wades-MacBook-Air.local>
2023-11-20 14:18:54 -0500

    redirect user if not logged in

--- src/Routes/Articles/Articles.js
+++ src/Routes/Articles/Articles.js
@@ -1,17 +1,28 @@
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import "./Articles.css";
 import Table from "../../Components/Table/Table";
 import Modal from "../../Components/Modal/Modal";
 import { useRecoilState } from "recoil";
 import { modalContentAtom } from "../../Atoms";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Articles() {
     const auth = getAuth();
+    const navigate = useNavigate();
     const [modalContent, setModalContent] = useRecoilState(modalContentAtom);
 
     const handleSignOut = () => {
         signOut(auth);
     };
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (!user) {
+                navigate("/table", { replace: true });
+            }
+        });
+    }, []);
 
     return (
         <>
